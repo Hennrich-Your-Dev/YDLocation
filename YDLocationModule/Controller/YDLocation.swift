282fcb07533c43d4199f154adc98a6bd9fc03e61
Douglas Hennrich <douglashennrich@yourdev.com.br>
2020-11-09 11:06:13 -0300

    Updating

--- YDLocationModule/Controller/YDLocation.swift
+++ YDLocationModule/Controller/YDLocation.swift
@@ -10,78 +10,88 @@ import UIKit
 import CoreLocation
 
 public class YDLocation: NSObject {
-    
-    // MARK: Properties
-    public static let shared = YDLocation()
-    
-    public var delegate: YDLocationDelegate?
-    
-    private var status: CLAuthorizationStatus = {
-       return CLLocationManager.authorizationStatus()
-    }()
-    
-    private let locationManager = CLLocationManager()
-    
-    // MARK: Init
-    public override init() {
-        super.init()
-        locationManager.delegate = self
+
+  // MARK: Properties
+  public static let shared = YDLocation()
+
+  public var delegate: YDLocationDelegate? {
+    didSet {
+      canTrigger = true
     }
-    
-    // MARK: Actions
-    private func requestAuthorization() {
-        locationManager.requestWhenInUseAuthorization()
-    }
-    
-    private func searchForCurrentLocation() {
-        locationManager.requestLocation()
-    }
-    
+  }
+
+  private var canTrigger = false
+
+  private var status: CLAuthorizationStatus = {
+    return CLLocationManager.authorizationStatus()
+  }()
+
+  private let locationManager = CLLocationManager()
+
+  // MARK: Init
+  public override init() {
+    super.init()
+    locationManager.delegate = self
+  }
+
+  // MARK: Actions
+  private func requestAuthorization() {
+    locationManager.requestWhenInUseAuthorization()
+  }
+
+  private func searchForCurrentLocation() {
+    locationManager.requestLocation()
+  }
+
 }
 
 // MARK: Public Actions
 extension YDLocation {
-    
-    public func getStatus() -> CLAuthorizationStatus {
-        return status
+
+  public func getStatus() -> CLAuthorizationStatus {
+    return status
+  }
+
+  public func start() {
+    if status == .notDetermined {
+      return requestAuthorization()
     }
-    
-    public func start() {
-        if status == .notDetermined {
-            return requestAuthorization()
-        }
-        
-        if status == .denied {
-            delegate?.permissionDenied()
-            return
-        }
-        
-        searchForCurrentLocation()
+
+    if status == .denied {
+      delegate?.permissionDenied()
+      return
     }
-    
+
+    searchForCurrentLocation()
+  }
+
 }
 
 extension YDLocation: CLLocationManagerDelegate {
-    
-    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        self.status = status
-        
-        if status == .authorizedWhenInUse {
-            searchForCurrentLocation()
-            
-        } else if status == .denied {
-            delegate?.permissionDenied()
-        }
-        
+
+  public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    self.status = status
+
+    if !canTrigger {
+      return
     }
 
-    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let currentLocation = locations.last {
-            delegate?.onLocation(currentLocation)
-        }
+    if status == .authorizedWhenInUse {
+      searchForCurrentLocation()
+
+    } else if status == .denied {
+      delegate?.permissionDenied()
     }
 
-    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        delegate?.locationError(error)
+  }
+
+  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    if let currentLocation = locations.last {
+      delegate?.onLocation(currentLocation)
     }
+  }
+
+  public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    delegate?.locationError(error)
+  }
 }
